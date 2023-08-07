8a48cebb5bdeb1293fd963f42935520c1a438fa3
beth <bethcassdy@gmail.com>
2023-08-07 17:27:52 -0400

    first table

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,31 +1,29 @@
 class LocationsController < ApplicationController
-
   def index
-    @locations = Locations.all
+    @locations = Location.all
     render json: @locations
   end
 
   def show
-    @location = Locations.find(params[:id])
+    @location = Location.find(params[:id])
     render json: @location
   end
 
   def create
     @location = Location.create(
-      location_name:params[:location_name]
-      zipcode:params[:zipcode]
-      menu:params[:menu]
+      location_name: params[:location_name],
+      zipcode: params[:zipcode],
+      menu: params[:menu],
     )
     render json: @location
   end
 
-
   def update
     @location = Location.find(params[:id])
     @location.update(
-      location_name:params[:location_name]
-      zipcode:params[:zipcode]
-      menu:params[:menu]
+      location_name: params[:location_name],
+      zipcode: params[:zipcode],
+      menu: params[:menu],
     )
     render json: @location
   end
@@ -37,5 +35,3 @@ class LocationsController < ApplicationController
     render json: @locations
   end
 end
-
-
