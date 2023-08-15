f9b45d91d53b6b9df7ff355e6edb44965abbaccb
beth <bethcassdy@gmail.com>
2023-08-15 10:07:15 -0400

    Updated Seeds File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,29 +1,40 @@
-# location1 = Location.create(location_name: "Samura Papa", zipcode: 11216, menu: "good")
+location1 = Location.create(location_name: "Samura Papa", zipcode: 11216, menu: "good")
 
-# location2 = Location.create(location_name: "Samura Mama", zipcode: 11211, menu: "nice")
+location2 = Location.create(location_name: "Samura Mama", zipcode: 11211, menu: "nice")
 
-# location3 = Location.create(location_name: "Samura Mama", zipcode: 11206, menu: "really cool")
+location3 = Location.create(location_name: "Samura Mama", zipcode: 11206, menu: "really cool")
 
-# zip1 = Zip.create(zip_number: "11216"),
-#        zip2 = Zip.create(zip_number: "11211"),
-# zip3 = Zip.create(zip_number: "11206")
-# zip4 = Zip.create(zip_number: "11221", x: 6, y: 8, d: 0)
-# zip5 = Zip.create(zip_number: "11201", x: 3, y: 8, d: 0)
-# zip6 = Zip.create(zip_number: "11205", x: 4, y: 8, d: 0)
-# zip7 = Zip.create(zip_number: "11222", x: 5, y: 10, d: 0)
-# zip8 = Zip.create(zip_number: "11237", x: 6, y: 9, d: 0)
-# zip9 = Zip.create(zip_number: "11233", x: 6, y: 7, d: 0)
-# zip10 = Zip.create(zip_number: "11212", x: 6, y: 7, d: 0)
-# zip11 = Zip.create(zip_number: "11203", x: 6, y: 5, d: 0)
-# zip12 = Zip.create(zip_number: "11215", x: 3, y: 6, d: 0)
-# --location4 = Location.create(location_name: "Kogane", zip_id: 5, menu: "https://www.koganeramenny.com/")
+zip1 = Zip.create(zip_number: "11216", x: 5, y: 7)
+zip3 = Zip.create(zip_number: "11206", x: 5, y: 8)
+zip4 = Zip.create(zip_number: "11221", x: 6, y: 8, d: 0)
+zip5 = Zip.create(zip_number: "11201", x: 3, y: 8, d: 0)
+zip6 = Zip.create(zip_number: "11205", x: 4, y: 8, d: 0)
+zip7 = Zip.create(zip_number: "11222", x: 5, y: 10, d: 0)
+zip8 = Zip.create(zip_number: "11237", x: 6, y: 9, d: 0)
+zip9 = Zip.create(zip_number: "11233", x: 6, y: 7, d: 0)
+zip10 = Zip.create(zip_number: "11212", x: 6, y: 7, d: 0)
+zip11 = Zip.create(zip_number: "11203", x: 6, y: 5, d: 0)
+zip12 = Zip.create(zip_number: "11215", x: 3, y: 6, d: 0)
+zip13 = Zip.create(zip_number: 11209, x: 1, y: 3, d: 0)
+zip14 = Zip.create(zip_number: 11220, x: 2, y: 4, d: 0)
+zip15 = Zip.create(zip_number: 11232, x: 2, y: 5, d: 0)
+zip16 = Zip.create(zip_number: 11231, x: 2, y: 7, d: 0)
+zip17 = Zip.create(zip_number: 11251, x: 4.5, y: 9, d: 0)
+zip18 = Zip.create(zip_number: 11211, x: 5, y: 9, d: 0)
+zip19 = Zip.create(zip_number: 11211, x: 5, y: 9, d: 0)
+zip20 = Zip.create(zip_number: 11218, x: 3.5, y: 5, d: 0)
+zip21 = Zip.create(zip_number: 11226, x: 5, y: 5, d: 0)
+zip22 = Zip.create(zip_number: 11238, x: 4.5, y: 7, d: 0)
+zip23 = Zip.create(zip_number: 11217, x: 3.5, y: 7, d: 0)
 
-# location5 = Location.create(location_name: "Ichiran", zip_id: 3, menu: "https://www.ichiranusa.com/menu/")
+--location4 = Location.create(location_name: "Kogane", zip_id: 5, menu: "https://www.koganeramenny.com/")
 
-# location6 = Location.create(location_name: "Naruto Ramen", zip_id: 12, menu: "https://www.narutoramenbrooklyn.com/")
+location5 = Location.create(location_name: "Ichiran", zip_id: 3, menu: "https://www.ichiranusa.com/menu/")
 
-# location7 = Location.create(location_name: "South Slope Ramen", zip_id: 12, menu: "https://south-slope-ramen.business.site/")
+location6 = Location.create(location_name: "Naruto Ramen", zip_id: 12, menu: "https://www.narutoramenbrooklyn.com/")
 
-# location8 = Location.create(location_name: "Nana Ramen", zip_id: 9, menu: "https://www.instagram.com/nana.ramen/")
+location7 = Location.create(location_name: "South Slope Ramen", zip_id: 12, menu: "https://south-slope-ramen.business.site/")
 
-# location9 = Location.create(location_name: "FUKUROU BROOKLYN", zip_id: 8, menu: "https://www.fukuroubrooklyn.getsauce.com/order-online?utm_source=GMB&utm_medium=Menu%20link&utm_id=Fukurou%20Brooklyn")
+location8 = Location.create(location_name: "Nana Ramen", zip_id: 9, menu: "https://www.instagram.com/nana.ramen/")
+
+location9 = Location.create(location_name: "FUKUROU BROOKLYN", zip_id: 8, menu: "https://www.fukuroubrooklyn.getsauce.com/order-online?utm_source=GMB&utm_medium=Menu%20link&utm_id=Fukurou%20Brooklyn")
